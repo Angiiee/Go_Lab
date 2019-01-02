3f96c79ae2b75e472aa830cbe9a0762983980137
Anhelina_Maretskaya <Anhelina_Maretskaya@epam.com>
2019-01-03 02:52:59 +0300

    change v2.4

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -15,20 +14,20 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 
-	db, err := sql.Open("mysql", "root:password@/notesdb")
+	//db, err := sql.Open("mysql", "root:password@/notesdb")
 
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer db.Close()
 
-	result, err := db.Exec("insert into notesdb.user_info (userName, description) values (?, ?)",
-		"Hello", "Hello World!")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(result.LastInsertId()) // id добавленного объекта
-	fmt.Println(result.RowsAffected()) // количество затронутых строк
+	//result, err := db.Exec("insert into notesdb.user_info (userName, description) values (?, ?)",
+	//	"Hello", "Hello World!")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(result.LastInsertId()) // id добавленного объекта
+	//fmt.Println(result.RowsAffected()) // количество затронутых строк
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
