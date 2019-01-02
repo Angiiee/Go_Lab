1d90b640636ebc127a8e4c33e2da944cd35ba405
Anhelina_Maretskaya <Anhelina_Maretskaya@epam.com>
2019-01-02 23:23:47 +0300

    change v2

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -10,6 +12,21 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+
+	db, err := sql.Open("mysql", "root:password@/notesdb")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	result, err := db.Exec("insert into notesdb.user_info (userName, description) values (?, ?)",
+		"Hello", "Hello World!")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result.LastInsertId()) // id добавленного объекта
+	fmt.Println(result.RowsAffected()) // количество затронутых строк
 
 	if port == "" {
 		//log.Fatal("$PORT must be set")
