b4de3d082e7c4ecf66fe5895c5b628813d2de1e7
Anhelina_Maretskaya <Anhelina_Maretskaya@epam.com>
2019-01-03 02:32:13 +0300

    add new dependency

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/russross/blackfriday"
 	"log"
 	"net/http"
 	"os"
@@ -41,6 +42,10 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.GET("/mark", func(c *gin.Context) {
+		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
 	})
 
 	router.Run(":" + port)
