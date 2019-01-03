e11e82e5c49b1031f1d1265c77ca8444fbe73142
Anhelina_Maretskaya <Anhelina_Maretskaya@epam.com>
2019-01-03 15:34:37 +0300

    change v3.15

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	http.HandleFunc("/note", processNote)
+	http.HandleFunc("/", processNote)
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
